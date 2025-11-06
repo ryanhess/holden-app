36b506818aa6da433ee6c2be4c68115fae33966c
ryanhess <hess29@gmail.com>
2025-11-05 23:23:14 -0500

    Set up a skeleton for async loading of the page as components become loadeddata from some data source. No database set up yet, data and the loading of it are simulated, but the ui is working.

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,10 +1,46 @@
+// server component by default
+'use server'
+import { simulateApiCall } from '@/lib/devtools'
+import { Suspense } from 'react'
+
 export async function generateMetadata({ params }) {
     const { id } = await params;
+    const { babyName } = await getUserData(id);
     return {
-        title: `User ${id} Profile`
+        title: `User ${babyName} Profile`
     };
 }
 
-export default function ProfilePage() {
-    return (<>specific user page</>)
+//ProfilePage must be async because Next made it a requirement
+//in order to use the params API
+export default async function ProfilePage({ params }) {
+    const { id } = await params;
+    return (
+        <Suspense fallback={<>Retrieving user data...</>}>
+            <UserDataDisplay id={id} />
+        </Suspense>
+    );
 }
+
+async function UserDataDisplay({ id }) {
+    const { userID, babyName, babyBirthday } = await getUserData(id);
+    return (
+        <ul>
+            <li>userID: {userID}</li>
+            <li>Baby's Name: {babyName}</li>
+        </ul>
+    );
+}
+
+async function getUserData(id) {
+    const data = {
+        userID: id,
+        babyName: 'Holden',
+        babyBirthday: new Date("10/19/2025")
+    };
+
+    await simulateApiCall(data, 2000);
+
+    return data;
+}
+
